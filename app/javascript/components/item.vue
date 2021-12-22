8ed10cec02ee172e8d767896b84f5c98262f4d8e
Grayden <graydenshand@gmail.com>
2021-12-22 14:15:34 -0500

    Fix webkit rounding of input underline, update Procfile

--- app/javascript/components/item.vue
+++ app/javascript/components/item.vue
@@ -5,7 +5,7 @@
 			<button class="h-4 w-4 mr-2 rounded-full bg-white border border-gray-300 flex justify-center items-center" v-on:click="deleteSelf">
 				<svg v-if="isDeleting" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
 			</button>
-			<input v-model="name" v-bind:id="`item-${item.id}-input`" type="text" class="bg-gray-50 w-full border-b border-transparent focus:outline-none focus:border-blue-500" v-on:keyup.enter="$emit('create-item', item.id)" />
+			<input v-model="name" v-bind:id="`item-${item.id}-input`" type="text" class="bg-gray-50 w-full border-b border-transparent rounded-none focus:outline-none focus:border-blue-500" v-on:keyup.enter="$emit('create-item', item.id)" />
 		</div>
 		<p class="text-xs text-red-500">{{error}}</p>
 	</div>
